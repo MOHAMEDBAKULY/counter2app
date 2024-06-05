d663681ce7feb5cd62868849e06efbab0666524e
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-06-05 21:08:23 +0300

    Magical counter app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Counter />
     </div>
   );
 }
 
-export default App;
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  return (
+    <>
+      <div className="header">
+        <h2>The Magical Date Counter</h2>
+        <input type="range" min="0" max="10" />
+        <label>{step}</label>
+      </div>
+
+      <div className="showdate">
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
+
+      <div className="footer">
+        <button onClick={() => setCount((c) => c - 1)}>Sub ➖</button>
+        <input type="text" value={count} />
+        <button onClick={() => setCount((c) => c + 1)}>Add ➕</button>
+      </div>
+    </>
+  );
+}
