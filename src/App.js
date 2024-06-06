49fbd05b45788ee58b088662f9f965cb9f5e4bbe
MOHAMED BAKULY <119895383+MOHAMEDBAKULY@users.noreply.github.com>
2024-06-06 04:58:37 +0300

    Using State the react way

--- src/App.js
+++ src/App.js
@@ -12,6 +12,11 @@ function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
 
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   const date = new Date();
   date.setDate(date.getDate() + count);
 
@@ -19,8 +24,14 @@ function Counter() {
     <>
       <div className="header">
         <h2>The Magical Date Counter</h2>
-        <input type="range" min="0" max="10" />
-        <label>{step}</label>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <label>Steps: {step}</label>
       </div>
 
       <div className="showdate">
@@ -35,10 +46,20 @@ function Counter() {
       </div>
 
       <div className="footer">
-        <button onClick={() => setCount((c) => c - 1)}>Sub ➖</button>
-        <input type="text" value={count} />
-        <button onClick={() => setCount((c) => c + 1)}>Add ➕</button>
+        <button onClick={() => setCount((c) => c - step)}>Sub ➖</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>Add ➕</button>
       </div>
+
+      {count !== 0 || step !== 1 ? (
+        <div className="reset">
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </>
   );
 }
